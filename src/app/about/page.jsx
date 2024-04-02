05bb116726c88afc803042b7c73f0de7ed6932b1
jul1998 <jguevara321@gmail.com>
2024-04-01 22:36:47 -0600

    added contact page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
-import Image from 'next/image'
-import styles from './about.module.css'
+import React from "react";
+import Image from "next/image";
+import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
-    <div>
-      <div className={styles.imgContainer}>
-      <Image src="/about.png" alt="about" width={500} height={500} />
-      </div>
-     
-    </div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h2 className={styles.subtitle}>About Agency</h2>
+        <h1  className={styles.title}>
+          We are a creative agency 
+        </h1>
+        <p className={styles.desc}>
+          We are a creative agency located in the heart of the city. We pride
+          ourselves on our innovative and creative solutions. Our team is
+          passionate about design and technology and we strive to deliver the
+          best results to our clients.
+        </p>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Years of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Years of experience</p>
+          </div>
+          <div className={styles.box}>
+            <h1>10 K+</h1>
+            <p>Years of experience</p>
+          </div>
+        </div>
 
-export default AboutPage
+      </div>
+      <div className={styles.imageContainer}>
+        <Image
+          src="/about.png"
+          alt="About Image"
+          fill
+          className={styles.image}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+   <div className={styles.container}>
+    <div className={styles.logo}></div>
+    <div className={styles.text}>
+      <p>© 2021 Next14 Starter. All rights reserved.</p>
+    </div>
+   </div>
   )
 }
 
