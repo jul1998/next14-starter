ed92279e0f693331a6d806e82a39ad1a5a5ec8d7
jul1998 <jguevara321@gmail.com>
2024-04-03 00:41:34 -0600

    added post

---
+++ src/components/postCard/PostCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './postCard.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+const PostCard = () => {
+  return (
+     <div className={styles.container}>
+        <div className={styles.top}>
+          <div className={styles.imgContainer}>
+            <Image src="https://images.unsplash.com/photo-1711639105861-f55ef78adc51?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D" alt="post" fill />
+          </div>
+          <span className={styles.date}>01.01.2024</span>
+        </div>
+        <div className={styles.bottom}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}> Lorem asdas sads</p>
+          <Link className={styles.link} href="/blog/post"> Read More</Link>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default PostCard
